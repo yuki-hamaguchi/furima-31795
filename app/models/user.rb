8dd6cf88f0cca248b15a03ac8d2bacc0d05d0d81
yuki-hamaguchi <amourcoeurbisnoir@icloud.com>
2020-11-22 09:59:27 +0900

    単体テストコードRSpec導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-  has_many :orders
+  #has_many :items
+  #has_many :orders
   
+  validates :nickname, presence: true, uniqueness: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :birth_date, presence: true
+ 
+ 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,14 @@
 ActiveRecord::Schema.define(version: 2020_11_20_022936) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
+    t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "last_name", null: false
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
     t.string "first_name_kana", null: false
-    t.string "user_birthdey", null: false
+    t.date "birth_date", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
