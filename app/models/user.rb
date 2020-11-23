b2957e08511978289910e4d336635de15089c6aa
yuki-hamaguchi <amourcoeurbisnoir@icloud.com>
2020-11-23 19:04:23 +0900

    userモデル単体テストコード修正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,21 +7,15 @@ class User < ApplicationRecord
   #has_many :items
   #has_many :orders
   
-  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
-  NAME_REGEX_KANA = /\A[ァ-ヶー－]+\z/
 
   with_options presence: true do
-      validates :nickname, uniqueness: true
-      validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/}
-    with_options NAME_REGEX do
-      validates :last_name
-      validates :first_name
-    end
-    with_options NAME_REGEX_KANA do
-      validates :last_name_kana
-      validates :first_name_kana
-    end
-      validates :birth_date
+    validates :nickname, uniqueness: true
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/}
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :birth_date
   end
  
 end
