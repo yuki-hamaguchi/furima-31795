1823a966d8f830113b36e2bf82baf85ed9ce90f1
yuki-hamaguchi <amourcoeurbisnoir@icloud.com>
2020-11-27 20:44:03 +0900

    商品詳細編集修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,10 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:edit, :show, :update]
   before_action :illegal_entry, only: [:edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
-    # 前回の実装にてマージして差分が上がらなかった部分
   end
 
   def new
@@ -21,14 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -39,10 +37,12 @@ class ItemsController < ApplicationController
   private
 
   def illegal_entry
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
   def item_params
     params.require(:item).permit(:image, :item_name, :info, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price).merge(user_id: current_user.id)
   end
