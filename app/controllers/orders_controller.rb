025e2cb982fa6d4e5ede2d32a5b3eebf42e80086
yuki-hamaguchi <amourcoeurbisnoir@icloud.com>
2020-12-03 17:00:03 +0900

    商品購入機能修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,11 +2,11 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create]
 
-
   def index
     @order_address = OrderAddress.new
-    if current_user == @item.user
-      redirect_to root_path
+    if @item.order .present?
+       current_user == @item.user
+       redirect_to root_path
     end
   end
 
@@ -25,6 +25,10 @@ class OrdersController < ApplicationController
 
   def address_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def illegal_entry
+    redirect_to root_path unless current_user == @item.user
   end
 
   def set_item
